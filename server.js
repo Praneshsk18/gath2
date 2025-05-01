a1d808132b24cbb2a1365ba95e7850d2246a142f
pranesh <praneshsenthilkumar18@gmail.com>
2025-05-01 11:19:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ const start = async () => {
         })
         await sequelize.authenticate();
         console.log("Database Connected");
-        await fastify.listen({port:3000})
+        await fastify.listen({port:3000,host:"0.0.0.0"});
     }
     catch(e){
 console.log(e);
